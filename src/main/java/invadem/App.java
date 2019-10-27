f3e4b29c91da0294a14869ee43483285d303a60f
andregeorgis <andregeorgis@gmail.com>
2019-10-27 18:48:06 +1100

    Revert "Implemented a basic structure and drew the Tank"
    This reverts commit 0b80414fd17fb8010d2e0697e7fabd9de3f5e485.

--- src/main/java/invadem/App.java
+++ src/main/java/invadem/App.java
@@ -1,34 +1,23 @@
 package invadem;
 
-import invadem.assets.Tank;
-
 import processing.core.PApplet;
-import java.util.List;
-import java.util.ArrayList;
 
 public class App extends PApplet {
-    final int WIDTH = 640;
-    final int HEIGHT = 480;
-
-    private Tank tank;
 
     public App() {
-        ;
+        //Set up your objects
     }
 
     public void setup() {
         frameRate(60);
-
-        this.tank = new Tank(loadImage("tank1.png"));
     }
 
     public void settings() {
-        size(WIDTH, HEIGHT);
+        size(640, 480);
     }
 
-    public void draw() {
-        background(0);
-        this.tank.draw(this);
+    public void draw() { 
+        //Main Game Loop
     }
 
     public static void main(String[] args) {
